2e57fd604a7fe8734586baeef431df8440fb608d
Катерина Акатовская <105228803+katerina-sys@users.noreply.github.com>
2022-06-19 13:28:56 +0300

    Update api_test.py
    change all tests

--- bp_api/tests/api_test.py
+++ bp_api/tests/api_test.py
@@ -1,42 +1,38 @@
 import pytest
+import requests
 
-import main
+import bp_api.views
 
 
 class TestApi:
     post_keys = {"poster_name", "poster_avatar", "pic", "content", "views_count", "likes_count", "pk"}
 
-    @pytest.fixture
-    def app_instance(self):
-        return main.app.test_client()
+    def test_all_posts_correct_status_code(self):
+        """Тест стаус-код для всех постов"""
+        response = requests.get("http://127.0.0.1:5000/api/posts")
+        assert response.status_code == 200
 
-    # Статус-код всех постов (тест)
-    def test_all_posts_correct_status(self, app_instance):
-        result = app_instance.get("/api/posts", follow_redirects=True)
-        assert result.status_code == 200
-
-    #Проверка есть ли ключи у всех постов
-    def test_all_posts_has_correct_keys(self, app_instance):
-        result = app_instance.get("/api/posts", follow_redirects=True)
-        list_of_posts = result.get_json()
+    def test_all_posts_has_correct_keys(self):
+        """Тест на проверку всех ключей у постов"""
+        result = requests.get("http://127.0.0.1:5000/api/posts")
+        list_of_posts = result.json()
 
         for post in list_of_posts:
             assert post.keys() == self.post_keys, "Неверные ключи у полученного словаря"
 
-
-    # Статус-код одного поста (тест)
-    def test_single_post_correct_status(self, app_instance):
-        result = app_instance.get("/api/posts/1/", follow_redirects=True)
+    def test_single_post_correct_status(self):
+        """Тест стаус-код для одного поста"""
+        result = requests.get("http://127.0.0.1:5000/api/posts/1/")
         assert result.status_code == 200
 
-    # Статус-код несуществующего поста (тест)
-    def test_post_is_none_correct_status_404(self, app_instance):
-        result = app_instance.get("/api/posts/0/", follow_redirects=True)
+    def test_post_is_none_correct_status_404(self):
+        """Статус код несуществующего поста"""
+        result = requests.get("http://127.0.0.1:5000/api/posts/0/")
         assert result.status_code == 404
 
-    # Есть ли у элемента нужные ключи
-    def test_single_post_has_correct_keys(self, app_instance):
-        result = app_instance.get("/api/posts/1/", follow_redirects=True)
-        post = result.get_json()
+    def test_single_post_has_correct_keys(self):
+        """Тест на проверку всех ключей у поста"""
+        result = requests.get("http://127.0.0.1:5000/api/posts/1/")
+        post = result.json()
         post_keys = set(post.keys())
         assert post_keys == self.post_keys
